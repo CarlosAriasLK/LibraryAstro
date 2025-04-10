a6184c879dee5024a94bc06f995cfaf5fb34ab8d
Carlos Arias <ceau922@gmail.com>
2025-04-10 17:00:57 -0500

    Finish books dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import vercelAdapter from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercelAdapter(),
   vite: {
-    plugins: [tailwindcss() ]
+    plugins: [tailwindcss() ],
   }
 });
